8a706fc208faab1cea7f035d92a88a9582648b82
2357gi <2357.gi@gmail.com>
2023-10-22 00:27:55 +0900

    環境変数をクライアントサイドでも読み込めるように接頭辞を付与

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 const LoginButton: React.FC = () => {
   // const clientId: string = process.env.DISCORD_CLIENT_ID || '1164915702059245598';
   // const redirectUri: string = process.env.DISCORD_REDIRECT_URI || 'http://localhost:3000/dashboard';
-  const clientId: string = process.env.DISCORD_CLIENT_ID || '1164915702059245598';
-  const redirectUri: string = process.env.DISCORD_REDIRECT_URI || 'http://localhost:3000/dashboard';
-  const scope: string = 'identify guilds';  // この部分は環境変数にしてもよい
+  const clientId: string = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || '1164915702059245598';
+  const redirectUri: string = process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI || 'http://localhost:3000/dashboard';
+  const scope: string = 'identify guilds';
 
 
   const encodedRedirectUri = encodeURIComponent(redirectUri);
